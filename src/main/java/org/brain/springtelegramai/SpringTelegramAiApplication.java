9a8a3d863bbd3beb6efeab708264954ea919e133
Vitalii Seniuk <vitaliiseniuk04@gmail.com>
2023-11-22 12:28:30 +0200

    Initial commit

---
+++ src/main/java/org/brain/springtelegramai/SpringTelegramAiApplication.java
@@ -0,0 +1,13 @@
+package org.brain.springtelegramai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTelegramAiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringTelegramAiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/brain/springtelegramai/SpringTelegramAiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.brain.springtelegramai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTelegramAiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
